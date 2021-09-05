b1eee4c1dd6c527b605f9bd8b02eb98579f1d32a
Neeraj Kumar <nk7075034@gmail.com>
2021-09-05 21:21:05 +0530

    Longest common subsequence problem done

--- Longest_Common_Subsequence.cpp
+++ Longest_Common_Subsequence.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 
+#define MAX_LENGTH 1000
+
 using namespace std;
 
 int longestCommonSubsequence(string, string, int, int);
+int LongestCommonSubsequence(string, string, int, int);
+
+int memory[MAX_LENGTH + 1][MAX_LENGTH + 1];
+int Memory[MAX_LENGTH + 1][MAX_LENGTH + 1];
 
 int main(void)
 {
     string a, b;
     cout << "Enter the two string a and b: ";
     cin >> a >> b;
+    for (int i = 0; i < MAX_LENGTH + 1; i++)
+    {
+        for (int j = 0; j < MAX_LENGTH + 1; j++)
+        {
+            memory[i][j] = -1;
+        }
+    }
     cout << "The length of longest common subsequence is: " << longestCommonSubsequence(a, b, a.length(), b.length()) << '\n';
+    cout << "The length of longest common subsequence is: " << LongestCommonSubsequence(a, b, a.length(), b.length()) << '\n';
     return 0;
 }
 
@@ -19,15 +33,47 @@ int longestCommonSubsequence(string a, string b, int m, int n)
     {
         return 0;
     }
+    if (memory[m][n] != -1)
+    {
+        return memory[m][n];
+    }
 
     if (a[m - 1] == b[n - 1])
     {
-        return 1 + longestCommonSubsequence(a, b, m - 1, n - 1);
+        return memory[m][n] = 1 + longestCommonSubsequence(a, b, m - 1, n - 1);
     }
     else
     {
         int longest1 = longestCommonSubsequence(a, b, m, n - 1);
         int longest2 = longestCommonSubsequence(a, b, m - 1, n);
-        return max(longest1, longest2);
+        return memory[m][n] = max(longest1, longest2);
     }
+}
+
+int LongestCommonSubsequence(string a, string b, int m, int n)
+{
+    for (int i = 0; i < n + 1; i++)
+    {
+        Memory[0][i] = 0;
+    }
+    for (int i = 0; i < m + 1; i++)
+    {
+        Memory[i][0] = 0;
+    }
+
+    for (int i = 1; i < m + 1; i++)
+    {
+        for (int j = 1; j < n + 1; j++)
+        {
+            if (a[i - 1] == b[j - 1])
+            {
+                Memory[i][j] = 1 + Memory[i - 1][j - 1];
+            }
+            else
+            {
+                Memory[i][j] = max(Memory[i][j - 1], Memory[i - 1][j]);
+            }
+        }
+    }
+    return Memory[m][n];
 }
